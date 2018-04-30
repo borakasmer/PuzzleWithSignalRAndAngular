ebdb12129cd263ebdb3e0b501aa7286c8fd4dbd9
Bora Kasmer <bora@borakasmer.com>
2018-05-01 00:57:49 +0300

    update...

--- puzzleservice/DAL/PuzzleDataContext.cs
+++ puzzleservice/DAL/PuzzleDataContext.cs
@@ -10,6 +10,6 @@ public class PuzzleDataContext : DbContext
     {
       base.OnConfiguring(optionsBuilder);
 
-      optionsBuilder.UseSqlServer("Server=tcp:devnot.database.windows.net,1433;Initial Catalog=DevnotDB;Persist Security Info=False;User ID=devnot;Password=Abc-def-ghi-jkl;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+      optionsBuilder.UseSqlServer("Server=******;Initial Catalog=DevnotDB;Persist Security Info=False;User ID=******;Password=******;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
   }
